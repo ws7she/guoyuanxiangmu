c9b6fe723444405323a92b3eebb8a17271b9d74c
ws7she <ws7she@gmail.com>
2017-03-07 23:58:18 +0800

    merge

--- webapp/js/game.js
+++ webapp/js/game.js
@@ -1,29 +1,67 @@
 $(function () {
     var chutouStatus = false, guaptStatus = false, zhaiptStatus = false;
+    var plantLock = false;
 
     var game = {
         init: function () {
+            this.wrapDom();
             this.initEvent();
+        },
+        wrapDom: function () {
+            var leftData, rightData;
+            $.ajax({
+                url: "http://121.199.52.79:8080/farm_grape/index.php/farm/farm_msgL",
+                data: ({
+                    u_Id: "13800000000",
+                }),
+                type: "post",
+                dataType: "json",
+                success: function (res) {
+                    leftData = res.content[0];
+                    document.getElementById('user-info').innerHTML = template('gameUser', leftData);
+                    document.getElementById('game-money').innerHTML = template('gameMoney', leftData);
+                },
+                error: function (res) {
+                    alert("接口问题")
+                }
+            })
+            $.ajax({
+                url: "http://121.199.52.79:8080/farm_grape/index.php/farm/farm_msgR",
+                data: ({
+                    u_Id: "13800000000",
+                }),
+                type: "post",
+                dataType: "json",
+                success: function (res) {
+                    rightData = res.content[0];
+                    console.log(res)
+                    document.getElementById('game-plant').innerHTML = template('gamePlant', rightData);
+                    if (res.code == "300") {
+                        for (var i = res.content.length - 1; i >= 0; i--) {
+                            if (res.content[i].f_State == 1) {
+                                $(".gy:eq(" + i + ")").attr("class", "gy one");
+                                $(".gy:eq(" + i + ")").css("transform", "rotate(0) skew(0)");
+                                $(".gy:eq(" + i + ")").html($("<div class='putao'></div>"));
+                            } else if (res.content[i].f_State == 2) {
+                                $(".gy:eq(" + i + ")").attr("class", "gy four");
+                                $(".gy:eq(" + i + ")").css("transform", "rotate(0) skew(0)");
+                                $(".gy:eq(" + i + ")").html($("<div class='kuwei'></div>"));
+                            } else if (res.content[i].f_State == 0) {
+
+                            }
+                        }
+                    }
+                },
+                error: function (res) {
+                    console.log(res)
+                }
+            })
         },
         initEvent: function () {
             $(".chutou").on("click", this.chutou);
             $(".guaputao").on("click", this.guapt);
             $(".zhaiputao").on("click", this.zhaipt);
-            $(".ld1").on("click", this.ld1);
-            $(".ld2").on("click", this.ld2);
-            $(".ld3").on("click", this.ld3);
-            $(".ld4").on("click", this.ld4);
-            $(".ld5").on("click", this.ld5);
-            $(".hj1").on("click", this.hj1);
-            $(".hj2").on("click", this.hj2);
-            $(".hj3").on("click", this.hj3);
-            $(".hj4").on("click", this.hj4);
-            $(".hj5").on("click", this.hj5);
-            $(".td1").on("click", this.td1);
-            $(".td2").on("click", this.td2);
-            $(".td3").on("click", this.td3);
-            $(".td4").on("click", this.td4);
-            $(".td5").on("click", this.td5);
+            $(".gy").on("click", this.plantEvent);
         },
         chutou: function () {
             chutouStatus = true;
@@ -34,78 +72,127 @@ $(function () {
         zhaipt: function () {
             zhaiptStatus = true;
         },
-        common: function (selector, main, land_id) {
+        common: function (selector, main) {
             var xiaoshu = $("<div class='putao'></div>"),
                 zhongshu = $("<div class='putao_2'></div>"),
                 dashu = $("<div class='putao_3'></div>"),
-                kuwei = $("<div class='kuwei'></div>");
+                kuwei = $("<div class='kuwei'></div>"),
+                mainValue = $(selector).data("index");
             if ($(selector).hasClass("zero")) {
-                var land_state = false;
-                var land_time = new Date().getTime();
                 if (chutouStatus) {
-                    $(selector).attr('class', main + " one");
-                    $(selector).css("transform", "rotate(0) skew(0)");
-                    $(selector).html(xiaoshu);
-                    chutouStatus = false;
-                    // $.ajax({
-                    //     url: "http://121.199.52.79:8080/index.php/farm/plant",
-                    //     data: ({
-                    //         u_Id: "13800000000",
-                    //         land_id: 1,
-                    //         land_state: "0",
-                    //         land_time: land_time
-                    //     }),
-                    //     type: "post",
-                    //     dataType: "JSON",
-                    //     success: function (data) {
-                    //         console.log(data);
-                            
-                    //         chutouStatus = false;
-                    //     },
-                    //     error: function (res) {
-                    //         console.log(res);
-                    //         alert("接口请求失败，请重试")
-                    //     }
-                    // })
+                    $.ajax({
+                        url: "http://121.199.52.79:8080/farm_grape/index.php/farm/plant",
+                        data: ({
+                            u_Id: "13800000000",
+                            f_Id: mainValue
+                        }),
+                        type: "post",
+                        dataType: "JSON",
+                        success: function (data) {
+                            if (data.code == "200") {
+                                $(selector).attr('class', main + " one");
+                                $(selector).css("transform", "rotate(0) skew(0)");
+                                $(selector).html(xiaoshu);
 
+                            } else if (data.code == "4003") {
+                                alert("种植失败")
+                            } else if (data.code == "4004") {
+                                alert("已经种植")
+                            } else if (data.code == "4005") {
+                                alert("已经枯萎")
+                            }
+
+                        },
+                        error: function (res) {
+                            console.log(res);
+                            alert("接口请求失败，请重试")
+                        }
+                    })
+                    chutouStatus = false;
                 }
-            }
-            // if ($(selector).hasClass("one")) {
-            //     if (guaptStatus) {
-            //         $(selector).attr("class", main + " two");
-            //         $(selector).css("transform", "rotate(0) skew(0)");
-            //         $(selector).html(zhongshu);
-            //         guaptStatus = false;
-            //     }
-            // }
-            if ($(selector).hasClass("one")) {
                 if (guaptStatus) {
-                    $(selector).attr("class", main + " three");
-                    $(selector).css("transform", "rotate(0) skew(0)");
-                    $(selector).html(dashu);
+                    alert("还没种植，不能挂")
                     guaptStatus = false;
                 }
                 if (zhaiptStatus) {
-                    // $(selector).attr("class", main + " four");
-                    // $(selector).css("transform", "rotate(0) skew(0)");
-                    // $(selector).html(kuwei);
-                    alert("不能摘！")
+                    alert("还没种植，不能摘！")
                     zhaiptStatus = false;
+                }
+            }
+            if ($(selector).hasClass("one")) {
+                if (guaptStatus) {
+                    $.ajax({
+                        url: "http://121.199.52.79:8080/farm_grape/index.php/farm/hang",
+                        data: ({
+                            f_Id: mainValue
+                        }),
+                        type: "post",
+                        dataType: "JSON",
+                        success: function (data) {
+                            if (data.code == "200") {
+                                $(selector).attr("class", main + " three");
+                                $(selector).css("transform", "rotate(0) skew(0)");
+                                $(selector).html(dashu);
+                            } else if (data.code == "4003") {
+                                alert("种植失败")
+                            } else if (data.code == "4005") {
+                                alert("已经枯萎")
+                            }
+
+                        },
+                        error: function (res) {
+                            console.log(res);
+                            alert("接口请求失败，请重试")
+                        }
+                    })
+                    guaptStatus = false;
+                }
+                if (zhaiptStatus) {
+                    alert("还没挂，不能摘！")
+                    zhaiptStatus = false;
+                }
+                if (chutouStatus) {
+                    alert("还没摘，不能种！")
+                    chutouStatus = false;
                 }
             }
             if ($(selector).hasClass("three")) {
+                 if (chutouStatus) {
+                    alert("已经成熟了，不能种！")
+                    chutouStatus = false;
+                }
                 if (guaptStatus) {
-                    // $(selector).attr("class", main + " three");
-                    // $(selector).css("transform", "rotate(0) skew(0)");
-                    // $(selector).html(dashu);
-                    alert("不能挂！")
+                    alert("已经成熟了，不能挂！")
                     guaptStatus = false;
                 }
                 if (zhaiptStatus) {
-                    $(selector).attr("class", main + " four");
-                    $(selector).css("transform", "rotate(0) skew(0)");
-                    $(selector).html(kuwei);
-                    zhaiptStatus = false;
+                    $.ajax({
+                        url: "http://121.199.52.79:8080/farm_grape/index.php/farm/hang",
+                        data: ({
+                            f_Id: mainValue
+                        }),
+                        type: "post",
+                        dataType: "JSON",
+                        success: function (data) {
+                            if (data.code == "200") {
+                                $(selector).attr("class", main + " four");
+                                $(selector).css("transform", "rotate(0) skew(0)");
+                                $(selector).html(kuwei);
+                            } else if (data.code == "4007") {
+                                alert("超出范围")
+                            } else if (data.code == "4006") {
+                                alert("未种植")
+                            } else if (data.code == "4005") {
+                                alert("已经枯萎")
+                            }
+                            zhaiptStatus = false;
+                        },
+                        error: function (res) {
+                            console.log(res);
+                            alert("接口请求失败，请重试")
+                        }
+                    })
+
                 }
             }
             if ($(selector).hasClass("four")) {
@@ -117,51 +204,9 @@ $(function () {
                 }
             }
         },
-        ld1: function () {
-            game.common(".ld1", "ld1", "ld1")
-        },
-        ld2: function () {
-            game.common(".ld2", "ld2")
-        },
-        ld3: function () {
-            game.common(".ld3", "ld3")
-        },
-        ld4: function () {
-            game.common(".ld4", "ld4")
-        },
-        ld5: function () {
-            game.common(".ld5", "ld5")
-        },
-        hj1: function () {
-            game.common(".hj1", "hj1")
-        },
-        hj2: function () {
-            game.common(".hj2", "hj2")
-        },
-        hj3: function () {
-            game.common(".hj3", "hj3")
-        },
-        hj4: function () {
-            game.common(".hj4", "hj4")
-        },
-        hj5: function () {
-            game.common(".hj5", "hj5")
-        },
-        td1: function () {
-            game.common(".td1", "td1")
-        },
-        td2: function () {
-            game.common(".td2", "td2")
-        },
-        td3: function () {
-            game.common(".td3", "td3")
-        },
-        td4: function () {
-            game.common(".td4", "td4")
-        },
-        td5: function () {
-            game.common(".td5", "td5")
-        },
+        plantEvent: function (selector) {
+            game.common($(this), "gy")
+        }
     }
     return game.init();
 });
