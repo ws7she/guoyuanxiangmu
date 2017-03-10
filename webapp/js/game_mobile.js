a4e3c861b5b132d9f7f5d50b4a7ca074fbad1212
ws7she <ws7she@gmail.com>
2017-03-10 08:19:29 +0800

    m

--- webapp/js/game_mobile.js
+++ webapp/js/game_mobile.js
@@ -13,47 +13,26 @@ $(function () {
             } else {
                 window.location = "index.html"
             }
-            $(".gylink").on("click", function () {
-                window.location.href = "guoyuan.html?uid=" + uid + "";
-            });
-            $(".hylink").on("click", function () {
-                window.location.href = "friend.html?uid=" + uid + "";
-            });
-            $(".jylink").on("click", function () {
-                window.location.href = "business.html?uid=" + uid + "";
-            });
-            $(".jflink").on("click", function () {
-                window.location.href = "shopping.html?uid=" + uid + "";
-            });
-            $(".yxlink").on("click", function () {
-                window.location.href = "game.html?uid=" + uid + "";
-            });
-            $(".grlink").on("click", function () {
-                window.location.href = "personal.html?uid=" + uid + "";
-            });
-            $(".lxlink").on("click", function () {
-                window.location.href = "friend.html?uid=" + uid + "";
-            })
         },
         wrapDom: function () {
             var leftData, rightData;
 
-            $.ajax({
-                url: "http://121.199.52.79:8080/farm_grape/index.php/farm/farm_msgL",
-                data: ({
-                    u_Id: uid,
-                }),
-                type: "post",
-                dataType: "json",
-                success: function (res) {
-                    leftData = res.content[0];
-                    document.getElementById('user-info').innerHTML = template('gameUser', leftData);
-                    document.getElementById('game-money').innerHTML = template('gameMoney', leftData);
-                },
-                error: function (res) {
-                    alert("接口问题")
-                }
-            })
+            // $.ajax({
+            //     url: "http://121.199.52.79:8080/farm_grape/index.php/farm/farm_msgL",
+            //     data: ({
+            //         u_Id: uid,
+            //     }),
+            //     type: "post",
+            //     dataType: "json",
+            //     success: function (res) {
+            //         leftData = res.content[0];
+            //         document.getElementById('user-info').innerHTML = template('gameUser', leftData);
+            //         document.getElementById('game-money').innerHTML = template('gameMoney', leftData);
+            //     },
+            //     error: function (res) {
+            //         alert("接口问题")
+            //     }
+            // })
             $.ajax({
                 url: "http://121.199.52.79:8080/farm_grape/index.php/farm/farm_msgR",
                 data: ({
@@ -64,26 +43,26 @@ $(function () {
                 success: function (res) {
                     rightData = res.content[0];
                     console.log(res)
-                    document.getElementById('game-plant').innerHTML = template('gamePlant', rightData);
+                    // document.getElementById('game-plant').innerHTML = template('gamePlant', rightData);
                     if (res.code == "300") {
                         for (var i = res.content.length - 1; i >= 0; i--) {
-                            $(".gy:eq(" + i + ")").attr("data-grape", res.content[i].f_grape)
-                            $(".gy:eq(" + i + ")").attr("data-index", res.content[i].f_Id);
-                            $(".gy:eq(" + i + ")").attr("data-status", res.content[14 - i].f_Val);
+                            $(".gy").eq(i).attr("data-grape", res.content[i].f_grape)
+                            $(".gy").eq(i).attr("data-index", res.content[i].f_Id);
+                            $(".gy").eq(i).attr("data-status", res.content[14 - i].f_Val);
                             if (res.content[i].f_State == 1) {
-                                $(".gy:eq(" + i + ")").attr("class", "gy one");
-                                $(".gy:eq(" + i + ")").css("transform", "rotate(0) skew(0)");
-                                $(".gy:eq(" + i + ")").html($("<div class='putao'></div>"));
+                                $(".gy").eq(i).attr("class", "gy one");
+                                $(".gy").eq(i).css("transform", "rotate(0) skew(0)");
+                                $(".gy").eq(i).html($("<div class='putao'></div>"));
                             } else if (res.content[i].f_State == 3) {
-                                $(".gy:eq(" + i + ")").attr("class", "gy four");
-                                $(".gy:eq(" + i + ")").css("transform", "rotate(0) skew(0)");
-                                $(".gy:eq(" + i + ")").html($("<div class='kuwei'></div>"));
+                                $(".gy").eq(i).attr("class", "gy four");
+                                $(".gy").eq(i).css("transform", "rotate(0) skew(0)");
+                                $(".gy").eq(i).html($("<div class='kuwei'></div>"));
                             } else if (res.content[i].f_State == 2) {
-                                $(".gy:eq(" + i + ")").attr("class", "gy three");
-                                $(".gy:eq(" + i + ")").css("transform", "rotate(0) skew(0)");
-                                $(".gy:eq(" + i + ")").html($("<div class='putao_3'></div>"));
+                                $(".gy").eq(i).attr("class", "gy three");
+                                $(".gy").eq(i).css("transform", "rotate(0) skew(0)");
+                                $(".gy").eq(i).html($("<div class='putao_3'></div>"));
                             } else if (res.content[i].f_State == 0) {
-                                $(".gy:eq(" + i + ")").attr("class", "gy zero");
+                                $(".gy").eq(i).attr("class", "gy zero");
                             }
                         }
                     }
@@ -135,6 +114,9 @@ $(function () {
                             f_Id: mainValue
                         }),
                         type: "post",
+                        headers: {
+                            "Content-Type": "application/json;charset=utf-8"
+                        },
                         dataType: "JSON",
                         success: function (data) {
                             console.log(data)
@@ -180,6 +162,9 @@ $(function () {
                         }),
                         type: "post",
                         dataType: "JSON",
+                        headers: {
+                            "Content-Type": "application/json;charset=utf-8"
+                        },
                         success: function (data) {
                             console.log(data)
                             if (data.code == "200") {
@@ -228,6 +213,9 @@ $(function () {
                         }),
                         type: "post",
                         dataType: "JSON",
+                        headers: {
+                            "Content-Type": "application/json;charset=utf-8"
+                        },
                         success: function (data) {
                             console.log(data)
                             if (data.code == "200") {
@@ -252,14 +240,6 @@ $(function () {
 
                 }
             }
-            // if ($(selector).hasClass("four")) {
-            //     if (chutouStatus) {
-            //         $(selector).attr("class", main + " zero");
-            //         $(selector).css("transform", "rotate(35deg) skew(-30deg)");
-            //         $(selector).html("");
-            //         chutouStatus = false;
-            //     }
-            // }
         },
         plantEvent: function (selector) {
             game.common($(this), "gy")
